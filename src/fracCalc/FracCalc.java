1e4bc7e31e3f77ad00b8e9db9e40fe9c4a71fc82
kaitlynchan <ununoctium829@gmail.com>
2016-12-02 08:54:47 -0800

    clean up

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -16,23 +16,13 @@ public class FracCalc {
 	        inputString = userInput.nextLine();        	
         }
     }	    
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
 	
     public static String produceAnswer(String input)
-    {	    	
+    {	// TODO: Implement this function to produce the solution to the input 	
     	String operand2 = "";
     	String operand = "";
     	String operator = "";
-    	
-        // TODO: Implement this function to produce the solution to the input 
-    	
+    		
     	//separates the input string into the three components (identifies the operator)
     	if (input.indexOf(" +") > 0){
 	    	operand =  input.substring(0, input.indexOf(" +"));
